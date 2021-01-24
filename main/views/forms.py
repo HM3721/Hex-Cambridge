0a1c4d0eaafb9379159e531357be74ee8148c95a
jll119 <jll119@ic.ac.uk>
2021-01-24 00:10:40 +0000

    front + back

--- main/views/forms.py
+++ main/views/forms.py
@@ -1,7 +1,7 @@
-"""Form object declaration."""
 from flask_wtf import FlaskForm
-from wtforms import StringField, TextField, SubmitField
-from wtforms.validators import DataRequired, Length
+from wtforms import StringField, TextField, SubmitField, PasswordField, TextAreaField, BooleanField
+from wtforms.validators import DataRequired, Length, ValidationError
+from wtforms.validators import DataRequired, Email, EqualTo, Length, ValidationError
 
 class ListingForm(FlaskForm):
     """Listing Form."""
@@ -30,6 +30,44 @@ class ListingForm(FlaskForm):
     
     #recaptcha = RecaptchaField()
     submit = SubmitField('Submit')
+
+class LoginForm(FlaskForm):
+    email = StringField(
+        'Email',
+        [DataRequired()]
+    )
+
+    password = StringField(
+        'Password',
+        [DataRequired()]
+    )
+    remember = BooleanField('remember me')
+
+    submit = SubmitField('Submit')
+
+class SignupForm(FlaskForm):
+
+    fname = StringField('First Name', [
+        DataRequired(message='PLease enter your first name'),
+        DataRequired()])
+    lname = StringField('Last Name', [
+        DataRequired(message='PLease enter your last name'),
+        DataRequired()])    
+    email = StringField('Email address', [
+        Email(message='Not a valid email address.'),
+        DataRequired()])
+    password = PasswordField('Password', [
+        DataRequired(message="Please enter a password."),
+        Length(min=6, message='Select a stronger password.')
+    ])
+
+    remember_me = BooleanField('Remember Me', default=False)
+    submit = SubmitField('Register')
+
+    def validate_email(self, email):
+        user = User.query.filter_by(email=email.data).first()
+        if user is not None:
+            raise ValidationError('Please use a different email address.')
 
 
     
